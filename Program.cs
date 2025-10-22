0684ef27f71a2826afe1412488b59a41d366261b
yaku5ov <133374988+yaku5ov@users.noreply.github.com>
2025-10-22 23:13:32 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace AviaCalc;
+
+class Program : MauiApplication
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+    static void Main(string[] args)
+    {
+        var app = new Program();
+        app.Run(args);
+    }
+}
